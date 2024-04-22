5447d5d8a4a39bd74861a1a21911fca9f1722641
Abhishek Gond <abhishekgond9670@gmail.com>
2024-04-22 10:19:39 +0530

    first commit

---
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,61 @@
+import MessageInput from "./MessageInput";
+import Messages from "./Messages";
+import { TiMessages } from "react-icons/ti";
+
+const MessageContainer = () => {
+  const noChatSelected = true;
+  return (
+    <div className="md:min-w-[450px] flex flex-col">
+      {noChatSelected ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          {/* <Header /> */}
+          <div className="bg-slate-500 px-4 py-2 mb-2">
+            <span className="label-text">To</span>{" "}
+            <span className="text-grey-900 font-bold">Abhishek</span>
+          </div>
+
+          <Messages />
+          <MessageInput />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default MessageContainer;
+
+const NoChatSelected = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="px-4 text-center sm:text-lg md:text-x1 text-grey-200 font-semibold flex flex-col items-center gap-2">
+        <p>Welcome 👋 Abhishek 🌺</p>
+        <p>Select a chat to start messaging</p>
+        <TiMessages className="text-3xl md:text-6xl text-center" />
+      </div>
+    </div>
+  );
+};
+
+// import MessageInput from "./MessageInput";
+// import Messages from "./Messages";
+
+// const MessageContainer = () => {
+//   return (
+//     <div className="md:min-w-[450] flex flex-col">
+//       <>
+//         {/* <Header /> */}
+//         <div className="bg-slate-500 px-4 py-2 mb-2">
+//           <span className="label-text">To</span>{" "}
+//           <span className="text-grey-900 font-bold">Abhishek</span>
+//         </div>
+
+//         <Messages />
+//         <MessageInput />
+//       </>
+//     </div>
+//   );
+// };
+
+// export default MessageContainer;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+// daisyUI config (optional)
+// https://daisyui.com/docs/config/
