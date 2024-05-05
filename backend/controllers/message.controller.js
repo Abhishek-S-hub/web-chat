942a5cb88980cd65356c2f231134c57fbb9d8cb0
Abhishek Gond <abhishekgond9670@gmail.com>
2024-05-05 20:27:53 +0530

    done

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,67 +1,67 @@
-import Conversation from '../models/conversation.model.js';
-import Message from '../models/message.model.js';
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
- try {
-    const {message} = req.body;
-    const {id: receiverId} = req.params;
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
     const senderId = req.user._id;
 
     let conversation = await Conversation.findOne({
-      participants: { $all: [senderId, receiverId]},
+      participants: { $all: [senderId, receiverId] }
     });
 
-
-    if(!conversation) {
+    if (!conversation) {
       conversation = await Conversation.create({
-         participants: [senderId, receiverId],
+        participants: [senderId, receiverId]
       });
     }
 
     const newMessage = new Message({
       senderId,
       receiverId,
-      message,
+      message
     });
 
-   if (newMessage) {
-    conversation.messages.push(newMessage._id);
-    
-   }
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
 
-   //await conversation.save();
-   //await newMessage.save();
-   // we will use this method to save both conversation and message in one transaction
+    //await conversation.save();
+    //await newMessage.save();
+    // we will use this method to save both conversation and message in one transaction
 
-   await Promise.all([conversation.save(), newMessage.save()]);
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
- } catch (error) {
-    console.log('Error in sendMessage controller: ', error.message)
-    res.status(500).json({ error: 'Internal server error' });
- }
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
-
 export const getMessage = async (req, res) => {
-
   try {
-    
-    const {id:userToChatId} = req.params;
+    const { id: userToChatId } = req.params;
     const senderId = req.user._id;
-    
+
     const conversation = await Conversation.findOne({
-      participants: { $all: [senderId, userToChatId]},
-  }).populate('messages');
+      participants: { $all: [senderId, userToChatId] }
+    }).populate("messages");
 
-  if(!conversation) return res.status(200).json([]);
+    if (!conversation) return res.status(200).json([]);
 
-  const messages = conversation.messages;
+    const messages = conversation.messages;
 
-  res.status(200).json(messages);
-
+    res.status(200).json(messages);
   } catch (error) {
-    console.log('Error in getMessages controller: ', error.message);
-    res.status(500).json({ error: "Internal server error"});
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -2,35 +2,30 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes from './routes/auth.routes.js' ;
-import messageRoutes from './routes/message.routes.js'; 
-import userRoutes from './routes/user.routes.js'; 
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(express.json());  // to parse the incoming requests with JSON payloads (fromm req.body)
-app.use(cookieParser());  
+app.use(express.json()); // to parse the incoming requests with JSON payloads (fromm req.body)
+app.use(cookieParser());
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-
 //app.get("/" , (req, res) => {
-    //root route http://localhost:5000/
-  //  res.send("HELLO WORLD!")
+//root route http://localhost:5000/
+//  res.send("HELLO WORLD!")
 //});
 
-
-
-
-app.listen(PORT, ()=> {
-    connectToMongoDB();
-    console.log(`server is running on port number ${PORT}`)
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`server is running on port number ${PORT}`);
 });
- 
